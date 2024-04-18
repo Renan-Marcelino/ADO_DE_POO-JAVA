4243c04aee065580e7572b3da045de66c6b7a639
Renan Marcelino <renanmarcelinodass@gmail.com>
2024-04-18 20:58:52 -0300

    Add files via upload

---
+++ tela_poo/src/main/java/com/mycompany/tela_poo/Produto.java
@@ -0,0 +1,62 @@
+package com.mycompany.tela_poo;
+
+public class Produto {
+    private int codigo;
+    private String descProduto;
+    private double preco;
+    private int quantidade;
+
+    public Produto(int codigo, String descProduto, double preco, int quantidade) {
+        this.codigo = codigo;
+        this.descProduto = descProduto; 
+        this.preco = preco;
+        this.quantidade = quantidade;
+        
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescProduto() {
+        return descProduto;
+    }
+
+    public void setDescProduto(String descProduto) {
+        this.descProduto = descProduto;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    public void listarDados(){
+        System.out.println("Codigo " + codigo + "Descrição " + descProduto + "Preço " + preco + "Quantidade " + quantidade);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "codigo: " + codigo + ", descProduto: " + descProduto + ", preco: " + preco + ", quantidade: " + quantidade + '}';
+    }
+    
+    
+    
+    
+}
+
---
+++ tela_poo/src/main/java/com/mycompany/tela_poo/Tela_POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tela_poo;
+
+/**
+ *
+ * @author renan.mssouza
+ */
+public class Tela_POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
